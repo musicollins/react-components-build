75a626daf8da4eb846e7184060c7f2b05d83c2de
MusiCollins <rafaelmilanes91@gmail.com>
2022-02-22 09:15:38 +0100

    Initial Commit. Next/React app created

---
+++ pages/index.jsx
@@ -0,0 +1,68 @@
+import { data } from "../SpeakerData";
+
+const index = () => {
+  return (
+    <div className="container speakers-list">
+      <div className="row">
+        {data.map(function (speaker) {
+          //destructuring properties in the data
+          const {
+            id,
+            bio,
+            first,
+            last,
+            favorite,
+            twitterHandle,
+            company,
+            sessions,
+          } = speaker;
+
+          return (
+            <div
+              key={id}
+              className="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-sm-12 col-xs-12"
+            >
+              <div className="card card-height p-4 mt-4">
+                {/*Image*/}
+                <div className="speaker-img d-flex flex-row justify-content-center align-items-center h-300">
+                  <img
+                    className="contain-fit"
+                    src={`/images/speaker-${id}.jpg`}
+                    width="300"
+                    alt={`${first} ${last}`}
+                  />
+                </div>
+                {/*Image*/}
+
+                {/*Speaker Demographics*/}
+                <div className="speaker-info">
+                  <div className="d-flex justify-content-between mb-3">
+                    <h3 className="text-truncate w-200">
+                      {first} {last}
+                    </h3>
+                  </div>
+                  <div>
+                    <p>
+                      {bio} {company} {twitterHandle} {favorite}
+                    </p>
+                  </div>
+                </div>
+                {/*Speaker Demographics*/}
+              </div>
+              {/*Speaker Session*/}
+              <div className="sessionBox card h-250">
+                <span className="session w-100">
+                  {sessions[0].title}{" "}
+                  <strong>Room: {sessions[0].room.name}</strong>
+                </span>
+              </div>
+              {/*Speaker Session*/}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default index;
