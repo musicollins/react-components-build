6d9294304249d8d4733e080b6f60b42c183ff276
MusiCollins <rafaelmilanes91@gmail.com>
2022-02-22 14:07:40 +0100

    Created Header, SpeakersToolbar

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ee491124309a8bc4"; }
+/******/ 		__webpack_require__.h = function() { return "890e9e3b481ad60e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
