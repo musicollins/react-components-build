3e4eb1be5e6ccf569f0e34257c82420074e6d392
MusiCollins <rafaelmilanes91@gmail.com>
2022-02-23 11:34:08 +0100

    Created Favorite component which handles state changes when click event is fired. The function handling the click exists in the SpeakersList component higher up in the hierarchy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import Header from "./Header";
 import Speakers from './Speakers'
 import {useState} from 'react'
 
+
 export default function App() {
   const [theme, setTheme] = useState("light")
 
--- src/components/Sessions.jsx
+++ src/components/Sessions.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Session from "./Session";
 
 function Sessions({ sessions }) {
-  console.log(sessions)
   return (
     <div className="sessionBox card h-250">
         <Session session={sessions[0]}/>
